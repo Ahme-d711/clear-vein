0747e7d01e45ae8f09547ea45b960bd475bc1651
Ahme-d711 <elgedawyahmed333@gmail.com>
2026-04-06 13:18:11 +0200

    refactor: remove redundant landing page sections to consolidate content into AboutSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Hero from "@/components/sections/Hero";
 import StatsSection from "@/components/sections/StatsSection";
-import Introduction from "@/components/sections/Introduction";
 import AboutSection from "@/components/sections/AboutSection";
-import OurApproach from "@/components/sections/OurApproach";
-import OurBackground from "@/components/sections/OurBackground";
-import CTASection from "@/components/sections/CTASection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Introduction />
-      <OurApproach />
-      <OurBackground />
-      <CTASection />
       <AboutSection />
       <StatsSection />
     </main>
